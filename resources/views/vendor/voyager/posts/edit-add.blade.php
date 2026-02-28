be04f986ef034aaab57778bfc9742f32f6dc769a
h_nakashima <lsd48x@gmail.com>
2026-02-28 19:39:32 +0900

    fixfix

---
+++ resources/views/vendor/voyager/posts/edit-add.blade.php
@@ -0,0 +1,6 @@
+{{--
+  Post BREAD 専用の edit-add。
+  item_id / title / body / cosme_categories など、Post 固有の表示・挙動を変えたい場合は
+  このファイルで @section('content') や @section('javascript') をオーバーライドする。
+--}}
+@extends('voyager::bread.edit-add')
